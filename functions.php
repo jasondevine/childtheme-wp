0716b7d46b04cfe42e2487bf2d9b4c8c035d441e
jasondevine <jason@zendroidthemes.com>
2021-02-06 22:15:06 +0000

    Got rid of header.php, sidebar.php and footer.php, not needed in a minimal child theme. Amended functions to enqueue the stylesheet from parent properly, with two options based on the parent theme configuration, whether it uses get_stylesheet_uri() or get_template_directory_uri()

--- functions.php
+++ functions.php
@@ -8,32 +8,33 @@
  * @package zt
  */
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+// If the parent theme loads its style using a function starting with get_stylesheet, such as get_stylesheet_directory() and get_stylesheet_directory_uri(), the child theme needs to load both parent and child stylesheets. Be sure to use the same handle name as the parent does for the parent styles.
+add_action('wp_enqueue_scripts', 'zt_child_theme_enqueue_styles');
+function zt_child_theme_enqueue_styles()
+{
+    $parenthandle = 'twentytwenty-style'; // This is 'twentytwenty-style' for the Twenty Twenty theme. Check the handle used in your parent theme and replace
+    $theme = wp_get_theme();
+    wp_enqueue_style(
+        $parenthandle,
+        get_template_directory_uri() . '/style.css',
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
 }
 
-if ( ! function_exists( 'zt_enqueue_styles' ) ) {
-	// Add enqueue function to the desired action.
-	add_action( 'wp_enqueue_scripts', 'zt_enqueue_styles' );
+// If the parent theme loads its style using a function starting with get_template, such as get_template_directory() and get_template_directory_uri(), the child theme needs to load just the child styles, using the parent’s handle in the dependency parameter.
 
-	/**
-	 * Enqueue Styles.
-	 *
-	 * Enqueue parent style and child styles where parent are the dependency
-	 * for child styles so that parent styles always get enqueued first.
-	 *
-	 * @since 1.0.0
-	 */
-	function zt_enqueue_styles() {
-		// Parent style variable.
-		$parent_style = 'parent-style';
-
-		// Enqueue Parent theme's stylesheet.
-		wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-		// Enqueue Child theme's stylesheet.
-		// Setting 'parent-style' as a dependency will ensure that the child theme stylesheet loads after it.
-		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
-	}
-}
+//  add_action( 'wp_enqueue_scripts', 'zt_child_theme_enqueue_styles' );
+//  function zt_child_theme_enqueue_styles() {
+// 	 $parenthandle = 'twentytwenty-style'; // This is 'twentytwenty-style' for the Twenty Twenty theme. Check the handle used in your parent theme and replace
+//      wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+//          array( 'twentytwenty-style' ),
+//          wp_get_theme()->get('Version') // this only works if you have Version in the style header
+//      );
+// }
